ee646ff32b8140c640cb6ab62183b1d832702b4c
Pablo Destefanis <malasanha@gmail.com>
2011-12-26 19:10:45 -0500

    Sample database config file added

---
+++ app/webroot/dataBase.php
@@ -0,0 +1,34 @@
+<?php
+
+//for expansion this should perhaps be a singleton with a database factory and connection pooling
+class dataBase {
+	
+	private $host = 'localhost';
+	private $user = 'username';
+	private $password = 'password';
+	private $database = 'database';
+	private $db;
+	
+	function __construct() {
+		@ $this->db = new mysqli($this->host, $this->user, $this->password, $this->database);
+
+		if (mysqli_connect_errno())
+		{
+			echo "Error: Could not connect to database. Please try again";
+			exit;
+		}
+	}    
+
+	function __destruct() {
+		$this->db->close();
+	}
+	
+	function query($sql) {
+		$result = $this->db->query($sql);
+		if (!$result)
+			die ("DB_ERROR: " . $this->db->error . "\n" . $sql);
+		return $result;
+	}
+
+}
+?>
