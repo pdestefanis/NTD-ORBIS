c10f2ef7a3e137cbf769a35902e6d58c0d58eeb1
fet mar <fetmar@gmail.com>
2012-08-07 09:26:46 -0400

    Different query methods for tree returns, add() batch saves approvals, and rest-like methods

--- app/controllers/approvals_controller.php
+++ app/controllers/approvals_controller.php
@@ -1,24 +1,294 @@
 <?php
 class ApprovalsController extends AppController {
 
+	const NOT_APPROVED = 0;
+	const APPROVED     = 1;
+	const ALL          = 2;
+
 	var $name = 'Approvals';
 	var $helpers = array('Html', 'Crumb', 'Javascript', 'Ajax');
 	var $components = array('RequestHandler', 'Access');
 	
-	function index() {
-		$this->Approval->recursive = 0;
-		$this->paginate['Approval'] = array('order' => 'Approval.created DESC');
-		$search = (empty($this->data['Search']['search'])?(isset($this->passedArgs[0])?$this->passedArgs[0]:$this->data['Search']['search']):$this->data['Search']['search']);
-		if (!empty($search) ) {
-				$this->paginate['Approval'] = array('order' => 'Approval.created ASC',
-										'conditions'=>array( 
-											"OR" => array("Messagereceived.rawmessage LIKE "=>"%".$search."%", 
-													"User.name LIKE" => "%".$search."%", 
-													"User.username LIKE" => "%".$search."%", 
-													)
-									));
-		} 
-		$this->set('approvals', $this->paginate());
+	function beforeFilter()
+	{
+		parent::beforeFilter();
+		$this->AuthExt->allow(array('restIndex', 'restApproval')); 
+	}
+	
+	function index($strFilter = null) 
+	{
+
+		App::import('Controller', 'Users');
+		App::import('Controller', 'Locations');
+		App::import('Controller', 'Stats');
+
+		$stats = new StatsController;
+		$stats->constructClasses();
+
+		$location = new LocationsController;
+		$location->constructClasses();
+
+		// get current location id
+		$users = new UsersController;
+		$users->constructClasses();
+		$user = $users->AuthExt->user();
+		$location_id = $user['User']['location_id'];
+
+		$user_reach = $user['User']['reach'];
+
+		if ($user_reach != 0)
+		{
+			for ( $i = 0; $i < $user_reach; $i++)
+			{
+				$this_location = $location->Location->read(null, $location_id);
+				if ( $this_location['Location']['parent_id'] != 0)
+					$location_id = $this_location['Location']['parent_id'];
+			}
+		}
+
+		$all = $location->getChildTree( $location_id, array(
+			'depth' => 1,
+			'parent' => null,
+			'approvalState' => self::ALL
+		));
+
+		$all = $location->setAggregates($all);
+		$all = $location->flattenTree($all);
+		$all = $location->arrayToHash($all);
+		$this->set('all', $all);
+
+		$approved = $location->getChildTree( $location_id, array(
+			'depth' => 1,
+			'parent' => null,
+			'approvalState' => self::APPROVED
+		));
+
+		$approved = $location->setAggregates($approved);
+		$approved = $location->flattenTree($approved);
+		$approved = $location->arrayToHash($approved);
+		$this->set('approved', $approved);
+
+	}
+
+	function pending()
+	{
+		
+		App::import('Controller', 'Users');
+		App::import('Controller', 'Locations');
+		App::import('Controller', 'Stats');
+
+		$stat = new StatsController;
+		$stat->constructClasses();
+		
+		$this->set( 'live_inventory', $stat->aggregatedInventory() );
+
+		$users = new UsersController;
+		$users->constructClasses();
+		$user = $users->AuthExt->user();
+		$location_id = $user['User']['location_id'];
+
+		$location = new LocationsController;
+		$location->constructClasses();
+
+		$user_reach = $user['User']['reach'];
+		
+		if ($user_reach != 0)
+		{
+			for ( $i = 0; $i < $user_reach; $i++)
+			{
+				$this_location = $location->Location->read(null, $location_id);
+				if ( $this_location['Location']['parent_id'] != 0)
+					$location_id = $this_location['Location']['parent_id'];
+			}
+		}
+
+		$all = $location->getChildTree( $location_id, array(
+			'depth' => 1,
+			'parent' => null,
+			'approvalState' => self::ALL
+		));
+
+		$all = $location->setAggregates($all);
+		$all = $location->flattenTree($all);
+		$all = $location->arrayToHash($all);
+		$this->set('all', $all);
+
+		$pending = $location->getChildTree( $location_id, array(
+			'depth' => 1,
+			'parent' => null,
+			'approvalState' => self::NOT_APPROVED
+		));
+
+		$pending = $location->setAggregates($pending);
+		$pending = $location->flattenTree($pending);
+		$pending = $location->arrayToHash($pending);
+		$this->set('pending', $pending);
+
+		$approved = $location->getChildTree( $location_id, array(
+			'depth' => 1,
+			'parent' => null,
+			'approvalState' => self::APPROVED
+		));
+
+		$approved = $location->setAggregates($approved);
+		$approved = $location->flattenTree($approved);
+		$approved = $location->arrayToHash($approved);
+		$this->set('approved', $approved);
+
+	}
+
+	function restIndex ($pId = 0)
+	{
+
+		App::import('Controller', 'Phones');
+		App::import('Controller', 'Users');
+		App::import('Controller', 'Locations');
+
+		$phones = new PhonesController;
+		$phones->constructClasses();
+		$phone = $phones->Phone->read(null, $pId);
+
+		$users = new UsersController;
+		$users->constructClasses();
+		$user = $users->AuthExt->user();
+
+		$location_id = $phone['Phone']['location_id'];
+
+		$location = new LocationsController;
+		$location->constructClasses();
+
+		$user_reach = $user['User']['reach'];
+
+		if ($user_reach != 0)
+		{
+			for ( $i = 0; $i < $user_reach; $i++)
+			{
+				$this_location = $location->Location->read(null, $location_id);
+				if ( $this_location['Location']['parent_id'] != 0)
+					$location_id = $this_location['Location']['parent_id'];
+			}
+		}
+
+		$location = new LocationsController;
+		$location->constructClasses();
+
+		$pending = $location->getChildTree( $location_id, array(
+			'depth' => 1,
+			'parent' => null,
+			'approvalState' => self::NOT_APPROVED
+		));
+
+		$pending = $location->setAggregates($pending);
+		$pending = $location->flattenTree($pending);
+		$pending = $location->arrayToHash($pending);
+
+		$quit = false;
+		echo "Pending \n";
+		foreach ($pending as $first)
+		{
+			if ($quit) continue;
+			foreach ($first as $key => $item)
+			{
+				$name = $item['total_items'][$key]['name'];
+				$quantity = $item['total_items'][$key]['quantity'];
+				echo "$name: $quantity";
+			}
+			$quit = true;
+		}	
+		$this->autoRender = false;
+	}
+	
+	function restApprove($pId=null, $iId=null)
+	{
+
+
+		App::import('Controller', 'Phones');
+		App::import('Controller', 'Users');
+		App::import('Controller', 'Locations');
+
+		$phones = new PhonesController;
+		$phones->constructClasses();
+		$phone = $phones->Phone->read(null, $pId);
+
+		$users = new UsersController;
+		$users->constructClasses();
+		$user = $users->AuthExt->user();
+
+		$location_id = $phone['Phone']['location_id'];
+
+		$location = new LocationsController;
+		$location->constructClasses();
+
+		$user_reach = $user['User']['reach'];
+
+		if ($user_reach != 0)
+		{
+			for ( $i = 0; $i < $user_reach; $i++)
+			{
+				$this_location = $location->Location->read(null, $location_id);
+				if ( $this_location['Location']['parent_id'] != 0)
+					$location_id = $this_location['Location']['parent_id'];
+			}
+		}
+
+		$location = new LocationsController;
+		$location->constructClasses();
+
+		$pending = $location->getChildTree( $location_id, array(
+			'depth' => 1,
+			'parent' => null,
+			'approvalState' => self::NOT_APPROVED
+		));
+
+		$pending = $location->setAggregates($pending);
+		$pending = $location->flattenTree($pending);
+		$pending = $location->arrayToHash($pending);
+
+		$quit = false;
+		$all_stats_ids = array();
+		echo "Approved\n ";
+		foreach ($pending as $first)
+		{
+			if ($quit) continue;
+			foreach ($first as $key => $item)
+			{
+
+				if ($item['total_items'][$key]['icode'] == $iId || $iId == "ALL")
+				{
+
+					$name = $item['total_items'][$key]['name'];
+					$quantity = $item['total_items'][$key]['quantity'];
+					$all_stats_ids = array_merge($all_stats_ids, $item['total_items'][$key]['stat_ids']);
+					echo "$name: $quantity";
+				}
+			}
+			$quit = true;
+		}	
+
+		//echo "\n stats ids: ". implode(",",$all_stats_ids);
+
+		$this->autoRender = false;
+	}
+
+	function approvalsByLocation()
+	{
+		$allApprovals = $this->Approval->find('all');
+		$approvalsByLocation = array();
+		foreach ($allApprovals as $approvalsStats) {
+			foreach ($approvalsStats['Stat'] as $approvalStat) {
+				if (isset($approvalsByLocation[$approvalStat['location_id']])) {
+					$approvalsByLocation[$approvalStat['location_id']][$approvalStat['item_id']] = array(
+						"created" => $approvalStat['created']
+					);
+				} else {
+					$approvalsByLocation[$approvalStat['location_id']] = array();
+					$approvalsByLocation[$approvalStat['location_id']][$approvalStat['item_id']] = array(
+						"created" => $approvalStat['created']
+					);
+				}
+			}
+		}
+		return $approvalsByLocation;
 	}
 
 	function view($id = null) {
@@ -31,8 +301,26 @@ class ApprovalsController extends AppController {
 		$this->set('items', $this->Approval->Stat->Item->find('list'));
 	}
 
-	private function add() {
+	public function add()
+	{
+		App::import('Controller', 'Users');
+		$users = new UsersController;
+		$users->constructClasses();
+		$user = $users->AuthExt->user();
+		$user_id = $user['User']['id'];
+		
+		if (isset($this->params['form']['stat_ids']))
+		{
+			$stat_ids = explode( ",", $this->params['form']['stat_ids'] );
+			$this->data = array(
+				"Approval" => array( "user_id" => $user_id ),
+				"Stat"     => array( "Stat" => $stat_ids )
+			);
+		}
+		
+
 		if (!empty($this->data)) {
+
 			$this->Approval->create();
 			if ($this->Approval->save($this->data)) {
 				$this->Session->setFlash(__('The approval has been saved', true));
@@ -40,11 +328,19 @@ class ApprovalsController extends AppController {
 			} else {
 				$this->Session->setFlash(__('The approval could not be saved. Please, try again.', true));
 			}
+
 		}
+		
 		$messagereceiveds = $this->Approval->Messagereceived->find('list');
 		$users = $this->Approval->User->find('list');
 		$stats = $this->Approval->Stat->find('list');
 		$this->set(compact('messagereceiveds', 'users', 'stats'));
+		
+	}
+
+	private function approve(){
+		$this->Session->setFlash('Approved successfully', 'flash_success');
+		$this->redirect( '/' );
 	}
 
 	private function edit($id = null) {
