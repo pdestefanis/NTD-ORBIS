16ab2dee85a39fcb1780f748d8fe88bd2686b0e3
fet mar <fetmar@gmail.com>
2012-08-07 09:41:49 -0400

    local configuration, change for your context

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -50,7 +50,7 @@
  *
  */
 	if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-		 define('CAKE_CORE_INCLUDE_PATH',  DS . 'var' . DS . 'www' . DS . 'html' . DS . 'orbis');	
+		 define('CAKE_CORE_INCLUDE_PATH',  DS . 'Users' . DS . 'fet' . DS . 'Sites' . DS . 'NTD');	
 		
 	}
 
